b9907e2805d4dc83dc8dfacac2e827d30140930d
İlayda Acarlar <acarlar.ilayda@hotmail.com>
2026-07-27 12:49:39 +0300

    feat: topbar app-root'a baglandi, rol degistirici calisir durumda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopbarComponent } from './shared/components/topbar/topbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'adaptif-egitim-platformu';
